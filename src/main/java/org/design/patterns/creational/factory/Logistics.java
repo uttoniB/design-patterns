2192e42b64676e2c8b01459707687daeb586f0ad
uttoniWork <uttoni.brandani@ges.inatel.br>
2025-08-03 11:52:56 -0300

    feat: Removendo comentario

--- src/main/java/org/design/patterns/creational/factory/Logistics.java
+++ src/main/java/org/design/patterns/creational/factory/Logistics.java
@@ -3,7 +3,6 @@ package org.design.patterns.creational.factory;
 public abstract class Logistics {
     public void planDelivery(){
 
-        //Would have to create a transport here, but we can use createTransport for that
         final Transport transport = createTransport();
         System.out.println("Planning delivery for transport " + transport);
 
